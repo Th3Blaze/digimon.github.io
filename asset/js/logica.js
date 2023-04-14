0cb405bd9fbb2376dcff6f02a7f06385c9915a52
Th3Blaze <patriciocardenaszapata@gmail.com>
2023-04-13 22:28:18 -0400

    modificacciones

--- asset/js/logica.js
+++ asset/js/logica.js
@@ -15,21 +15,11 @@ function crearTabla(resp) {
                 <td><img src="${datoTemporal.img}" alt=""></td>
                 <td>${datoTemporal.level}</td>
             `
-
-        listado.createElement +=
-            `
-            <option value="${datoTemporal.name}">${datoTemporal.name}</option> 
-            `
-            
     }
 
 }
 // mostrar ocultar
 function dgmListado() {
     document.getElementById('listar').style.display = "block";
-    document.getElementById('buscar').style.display = "none";
 }
-function dgmBuscar() {
-    document.getElementById('buscar').style.display = "block";
-    document.getElementById('listar').style.display = "none";
-}
+
